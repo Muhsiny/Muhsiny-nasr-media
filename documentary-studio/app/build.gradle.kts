2955b7c3d05e71f2560de433ee964749e82cff4a
Muhsiny <138768284+Muhsiny@users.noreply.github.com>
2026-09-07 18:28:35 +0430

    Bump Documentary Studio to v1.0.0

--- documentary-studio/app/build.gradle.kts
+++ documentary-studio/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "af.muhsiny.docstudio"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 10
+        versionName = "1.0.0"
     }
 
     compileOptions {
